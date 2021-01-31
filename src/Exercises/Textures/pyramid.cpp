6cba5c6defa752d919d27d97d266b4fb52d2b60a
mblasiak <milblasiak@gmail.com>
2021-01-31 15:19:22 +0100

    [Texture] Remove redundant vertices

--- src/Exercises/Textures/pyramid.cpp
+++ src/Exercises/Textures/pyramid.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <3rdParty/src/stb/stb_image.h>
+#include <iostream>
 #include "pyramid.h"
 
 Pyramid::Pyramid() {
@@ -14,7 +15,6 @@ Pyramid::Pyramid() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 
-
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_[0]);
@@ -26,9 +26,19 @@ Pyramid::Pyramid() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
                           reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
 
+//    Texture
     int width, height, n_channels;
-    auto texture_filename = std::string(PROJECT_DIR) + "/Textures/multicolor.png";
-    uint8_t  *data = stbi_load(texture_filename, &width, &height, &n_channels, 0);
+    auto texture_filename = std::string(PROJECT_DIR) + "/multicolor.png";
+    uint8_t *data = stbi_load(texture_filename.c_str(), &width, &height, &n_channels, 0);
+    if (data == nullptr) {
+        std::cerr << "Error loading texture";
+    }
+    glGenTextures(1, &diffuse_texture_);
+    glBindTexture(GL_TEXTURE_2D, diffuse_texture_);
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB, width, height,0, GL_RGB, GL_UNSIGNED_BYTE,data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
 }
 
